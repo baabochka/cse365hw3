029aab9dc3d4e26e3c6f0f675ca084768f252d81
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-03-07 19:21:29 -0500

    fixing typo

--- routes/emit.js
+++ routes/emit.js
@@ -14,7 +14,7 @@ router.post('/', function(req, res, next) {
             var my_msg = req.body.msg;
 
             ch.assertExchange(ex, 'direct', {durable: false});
-            ch.publish(ex, my_keys, new Buffer(my_msg));
+            ch.publish(ex, my_key, new Buffer(my_msg));
             console.log(" [x] Sent %s: '%s'", my_key, my_msg);
             res.json({ key: my_key, msg: my_msg});
         });
